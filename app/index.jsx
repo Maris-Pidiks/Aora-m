761013d3b36db020801b63c39b7bdcd34fb3cfa1
Maris-Pidiks <maris@divikiwi.lv>
2025-06-19 08:09:51 +0300

    add assets and constants, not useFont hook yet

--- app/index.jsx
+++ app/index.jsx
@@ -1,25 +1,20 @@
 import { Link } from "expo-router"
-import { Image, ImageBackground, StatusBar, Text, View } from 'react-native'
+import { Image, StatusBar, Text, View } from 'react-native'
 
 const RootLayout = () => {
   return (
-    <View style={{ flex: 1 }}>
+    <View className="items-center justify-center flex-1 bg-primary">
       <StatusBar barStyle="light-content" />
 
-      <ImageBackground
-        source={require("../assets/images/bgg.jpg")}
-        className="items-center justify-center flex-1 w-full h-full"
-      >
-        <Image
-          source={require("../assets/images/react-logo.png")}
-          className="w-[130px] h-[130px] mb-4.5"
-        />
-        <Text className="px-10 py-2 my-3 text-3xl text-white bg-red-500">MY FIRST</Text>
-        <Text className="text-2xl font-bold text-white">MOBILE APP</Text>
-        <Link href="/profile" className="text-gray-500 mt-2.5 ">
-          Go to profile
-        </Link>
-      </ImageBackground>
+      <Image
+        source={require("../assets/images/logo.png")}
+        className="w-[170px] h-[50px] mb-10"
+      />
+      <Text className="text-2xl font-bold text-white">MY FIRST</Text>
+      <Text className="mb-5 text-2xl font-bold text-white">MOBILE APP</Text>
+      <Link href="/profile" className="mt-4 mb-5 text-gray-500 ">
+        Go to profile
+      </Link>
     </View>
   )
 }
